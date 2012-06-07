673196d0b1a2c366ab1f12d6e8321234b94d326b
Ati <tordaga@gmail.com>
2012-06-07 16:48:28 +0200

    creating a tree

--- main.r
+++ main.r
@@ -1,26 +1,70 @@
-class Interpreter
-	attr_accessor :query
- 
- def initialize()
- @query = String.new
- end
+class TreeNode
+  attr_accessor :label, :nodes 
 
- def getfrominput#Get the query from the command line input
+  def initialize(label)
+  @label = label
+  @nodes = Array.new
+  end
+
+  def addnode(node)
+  @nodes.push node
+  end
+
+  def printout
+  print label, "("
+  @nodes.each { |node|
+  node.printout
+  }
+  print ")"
+  end
+
+end
+
+class Interpreter
+	attr_accessor :query, :rootnode
+ 
+  def initialize()
+  @query = String.new
+  @rootnode = TreeNode.new("rootnode")
+  end
+
+  def getfrominput#Get the query from the command line input
   puts "Please enter your query!"
   input = gets
   input.chomp!
   @query.concat(input)
- end
+  createtree(@rootnode)
+  end
 
- def getfromfile(path)#Get the query from a file
- File.open(path).each { |line|
+  def getfromfile(path)#Get the query from a file
+  File.open(path).each { |line|
     @query.concat(line)
- }
- end
+  }
+  createtree(@rootnode)
+  end
 
- def runquery
- puts(@query)
- end
+  def createtree(currentnode)#Make a tree from the commands
+  command = String.new
+  i = 0
+  while i < @query.size do
+	if(@query[i] == 59)#Stands for ;
+		currentnode.addnode(TreeNode.new(command))
+		command = String.new
+	else
+		command.concat(@query[i])
+	end
+	i += 1
+  end
+  end
+
+
+  def runquery
+  puts(@query)
+  end
+
+  def printtree
+  @rootnode.printout
+  end
 
 end
 
@@ -30,5 +74,7 @@ if(ARGV[0] == nil)
 else
 	main.getfromfile(ARGV[0])
 	end
-main.runquery
+#main.runquery
+main.printtree
+
 
