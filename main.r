c2b413078ff5b8f5e569cebc3ce21944a9aed61d
Zefirot <tordaga@gmail.com>
2012-07-05 00:03:57 +0200

    back on track
    expect some major updates in the next few weeks. i started implementing
    the new semantics

--- main.r
+++ main.r
@@ -1,8 +1,9 @@
 class TreeNode
-  attr_accessor :label, :nodes 
+  attr_accessor :type, :value, :nodes 
 
-  def initialize(label)
-  @label = label
+  def initialize(type, value)
+  @type = type
+  @value = value
   @nodes = Array.new
   end
 
@@ -10,8 +11,9 @@ class TreeNode
   @nodes.push node
   end
 
+  #Only test purpose
   def printout
-  print label, "("
+  print value, "("
   @nodes.each { |node|
   node.printout
   }
@@ -22,10 +24,15 @@ end
 
 class Interpreter
 	attr_accessor :query, :rootnode
+
+  #ascii constants
+  ENDLINE = 10.chr
+  SPACE = 32.chr
+  COMMENT = "#"#hash
  
   def initialize()
   @query = String.new
-  @rootnode = TreeNode.new("rootnode")
+  @rootnode = TreeNode.new("root", "rootnode")
   end
 
   def getfrominput#Get the query from the command line input
@@ -37,22 +44,23 @@ class Interpreter
   end
 
   def getfromfile(path)#Get the query from a file
-  file = File.open(path, "rb")
+  file = File.open(path, "r:ascii")#Trying to prevent ambiguity by using ascii encoding
   @query = file.read
   createtree(@rootnode)
   end
 
   def createtree(currentnode, query = nil)#Make a tree from the commands
   command = String.new
-  if(query == nil)
+  if(query == nil)#If no query string supplied it is the tree node
 	query = @query
 	end
   i = 0
-  while i < query.size do
-	if(query[i] == 59)#59 stands for ;
-		currentnode.addnode(TreeNode.new(command))
-		command = String.new
 
+  while i < query.size do
+	i = ignorespaces(i)
+
+	if(query[i] == COMMENT)
+	i = nextline(i)
 	elsif(query[i, 3].upcase.eql? "FOR")
 	     #Process "for" loop
 		condition = String.new #The conditions of the foor loop
@@ -75,23 +83,40 @@ class Interpreter
 			end
 		end #while
 		#Make the for loop tree
-		fornode = TreeNode.new(condition)
+		fornode = TreeNode.new("loop", condition)
 		createtree(fornode, body)
 		currentnode.addnode(fornode)
 
-	else
-		command.concat(query[i])
+	else#Primitive
+		command = String.new
+		while !(query[i] == ENDLINE) do
+		  command.concat(query[i])
+		  i += 1
+		  end
+		if(command.size > 0)#skip empty lines
+		  currentnode.addnode(TreeNode.new("primitive", command))
+		  end
+
 	end #if
 	i += 1
   end #while
   end #createtree
 
-
-  def runquery
-  puts(@query)
+  def ignorespaces(pos)#Jump to the next character
+  while query[pos] == SPACE do
+    pos += 1
+    end
+  return pos
   end
 
-  def printtree
+  def nextline(pos)#Jump to the next line
+  while query[pos] != ENDLINE do
+    pos += 1
+    end
+  return pos
+  end
+
+  def printtree#Test
   @rootnode.printout
   end
 
@@ -103,7 +128,5 @@ if(ARGV[0] == nil)
 else
 	main.getfromfile(ARGV[0])
 	end
-#main.runquery
 main.printtree
-
 
